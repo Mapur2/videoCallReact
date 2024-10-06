256908bfe8c96bc2c491ca63a003764cda1fdf30
Mapur2 <rupammodak1@gmail.com>
2024-10-06 22:36:41 +0530

    Deployed on vercel

--- client/src/context/SocketProvider.jsx
+++ client/src/context/SocketProvider.jsx
@@ -8,7 +8,7 @@ export const useSocket=()=>{
 }
 
 export const SocketProvider = (props)=>{
-    const socket = useMemo(()=>io('localhost:8000'),[])
+    const socket = useMemo(()=>io('videocallreact-production.up.railway.app'),[])
     return (
         <SocketContext.Provider value={socket}>
             {
